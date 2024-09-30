035ba13ee827ab98bbada6389f3c897cc868a2fa
Enzo Damascena <enzodm.dev@gmail.com>
2024-09-29 22:10:24 -0300

    chore: Update code formatting in App.tsx file

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
 
