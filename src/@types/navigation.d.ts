85f6ebb5d7d6b90e62d2f4b9bf3fae7a04014de4
Enzo Damascena <enzo.damascena@defensoria.mg.def.br>
2024-09-30 17:41:52 -0300

    feat: Add LocationDetails route to RootParamList

--- src/@types/navigation.d.ts
+++ src/@types/navigation.d.ts
@@ -2,6 +2,7 @@ export declare global {
   namespace ReactNavigation {
     interface RootParamList {
       Locations: undefined;
+      LocationDetails: { id: number };
       Login: undefined;
     }
   }
