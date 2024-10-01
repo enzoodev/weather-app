7eb49da7384b1d2cac72f26e38af53342cdd2b9d
Enzo Damascena <enzo.damascena@defensoria.mg.def.br>
2024-10-01 15:24:53 -0300

    feat: Add language support for English and Portuguese

---
+++ src/lib/language/i18n.ts
@@ -0,0 +1,30 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import { getLocales } from 'expo-localization';
+
+import { SupportedLanguages } from './SupportedLanguages';
+import en from './json/en.json';
+import pt from './json/pt.json';
+
+const resources = {
+  en: {
+    translation: en,
+  },
+  pt: {
+    translation: pt,
+  },
+};
+
+const [firstLocale] = getLocales();
+
+i18n.use(initReactI18next).init({
+  resources,
+  lng: firstLocale?.languageCode ?? SupportedLanguages.en,
+  fallbackLng: SupportedLanguages.en,
+  compatibilityJSON: 'v3',
+  interpolation: {
+    escapeValue: false,
+  },
+});
+
+export default i18n;
