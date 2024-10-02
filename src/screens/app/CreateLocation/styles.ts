977350fa1a927dae0cd08a354ebb356dddf0bba0
Enzo Damascena <enzo.damascena@defensoria.mg.def.br>
2024-10-02 15:19:40 -0300

    feat: Add CreateLocation screen and styles

---
+++ src/screens/app/CreateLocation/styles.ts
@@ -0,0 +1,16 @@
+import styled, { css } from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  ${({ theme }) => css`
+    padding: ${theme.layout[4]};
+    background-color: ${theme.colors.background};
+  `}
+`;
+
+export const Content = styled.View`
+  flex: 1;
+  ${({ theme }) => css`
+    gap: ${theme.layout[4]};
+  `}
+`;
