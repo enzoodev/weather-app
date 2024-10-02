1242843746ec239232a1fe79d2f3706079ec4e6b
Enzo Damascena <enzo.damascena@defensoria.mg.def.br>
2024-10-02 15:19:31 -0300

    refactor: Update createLocationSchema to use i18next for validation messages

--- src/schemas/location/createLocationSchema.ts
+++ src/schemas/location/createLocationSchema.ts
@@ -1,17 +1,9 @@
 import * as yup from 'yup';
-import { useTranslation } from 'react-i18next';
+import { t } from 'i18next';
 
-export const useCreateLocationSchema = () => {
-  const { t } = useTranslation();
+export const createLocationSchema = yup.object().shape({
+  state: yup.string().required(t('validation.state_required')),
+  city: yup.string().required(t('validation.city_required')),
+});
 
-  const createLocationSchema = yup.object().shape({
-    state: yup.string().required(t('validation.state_required')),
-    city: yup.string().required(t('validation.city_required')),
-  });
-
-  return createLocationSchema;
-};
-
-export type TCreateLocationSchema = yup.InferType<
-  ReturnType<typeof useCreateLocationSchema>
->;
+export type TCreateLocationSchema = yup.InferType<typeof createLocationSchema>;
