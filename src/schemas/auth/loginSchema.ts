d5c6c5d434282b80b0872336b5e84618a6868b74
Enzo Damascena <enzo.damascena@defensoria.mg.def.br>
2024-10-02 12:10:10 -0300

    refactor: Update validation messages in loginSchema.ts

--- src/schemas/auth/loginSchema.ts
+++ src/schemas/auth/loginSchema.ts
@@ -1,4 +1,5 @@
 import * as yup from 'yup';
+import { t } from 'i18next';
 import { authMock } from '@/mock/authMock';
 
 const { email } = authMock.user;
@@ -7,13 +8,16 @@ const fakePassword = '123456';
 export const loginSchema = yup.object().shape({
   email: yup
     .string()
-    .email('Email válido')
-    .required('O email é obrigatório')
-    .oneOf([email], `O email deve ser ${email}`),
+    .email(t('validation.email_valid'))
+    .required(t('validation.email_required'))
+    .oneOf([email], t('validation.email_must_be', { email })),
   password: yup
     .string()
-    .required('A senha é obrigatória')
-    .oneOf([fakePassword], `A senha deve ser ${fakePassword}`),
+    .required(t('validation.password_required'))
+    .oneOf(
+      [fakePassword],
+      t('validation.password_must_be', { password: fakePassword }),
+    ),
 });
 
 export type TLoginSchema = yup.InferType<typeof loginSchema>;
