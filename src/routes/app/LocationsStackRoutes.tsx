a89d471550540124dcc9673fe4a5b84c27273324
Enzo Damascena <enzo.damascena@defensoria.mg.def.br>
2024-10-02 15:19:07 -0300

    feat: Add CreateLocation screen to LocationsStackRoutes

--- src/routes/app/LocationsStackRoutes.tsx
+++ src/routes/app/LocationsStackRoutes.tsx
@@ -1,6 +1,7 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import { Locations } from '@/screens/app/Locations';
+import { CreateLocation } from '@/screens/app/CreateLocation';
 
 const { Navigator, Screen } = createNativeStackNavigator();
 
@@ -13,6 +14,7 @@ export const LocationsStackRoutes: React.FC = () => {
       initialRouteName="Locations"
     >
       <Screen name="Locations" component={Locations} />
+      <Screen name="CreateLocation" component={CreateLocation} />
     </Navigator>
   );
 };
