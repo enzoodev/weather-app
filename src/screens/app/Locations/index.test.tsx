ec234d042f66182fc179a0c0337ca1baec148a74
Enzo Damascena <enzo.damascena@defensoria.mg.def.br>
2024-10-07 11:51:59 -0300

    test: Update Locations component to use i18next for localization

--- src/screens/app/Locations/index.test.tsx
+++ src/screens/app/Locations/index.test.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { t } from 'i18next';
+import { I18nextProvider } from 'react-i18next';
 import { render, screen } from '@testing-library/react-native';
-import { useLocations } from '@/features/locations';
 import { NavigationContainer } from '@react-navigation/native';
 import { ThemeProvider } from 'styled-components/native';
+import { useLocations } from '@/features/locations';
 import { theme } from '@/theme';
+import i18n from '@/lib/language/i18n';
 import { Locations } from './index';
+
+jest.mock('expo-localization', () => ({
+  getLocales: jest.fn(() => [{ languageCode: 'en' }]),
+}));
 
 jest.mock('@/features/locations', () => ({
   useLocations: jest.fn(),
@@ -12,9 +19,11 @@ jest.mock('@/features/locations', () => ({
 
 const renderWithProviders = (component: React.ReactNode) => {
   return render(
-    <NavigationContainer>
-      <ThemeProvider theme={theme}>{component}</ThemeProvider>
-    </NavigationContainer>,
+    <I18nextProvider i18n={i18n}>
+      <NavigationContainer>
+        <ThemeProvider theme={theme}>{component}</ThemeProvider>
+      </NavigationContainer>
+    </I18nextProvider>,
   );
 };
 
@@ -35,13 +44,13 @@ describe('Locations Component', () => {
   it('renders correctly', () => {
     renderWithProviders(<Locations />);
 
-    expect(screen.getByText(/location.title/i)).toBeTruthy();
+    expect(screen.getByText(t('location.title'))).toBeTruthy();
   });
 
   it('displays empty location message when there are no locations', () => {
     renderWithProviders(<Locations />);
 
-    expect(screen.getByText(/location.empty_location/i)).toBeTruthy();
+    expect(screen.getByText(t('location.empty_location'))).toBeTruthy();
   });
 
   it('calls fetchLocations on mount', () => {
